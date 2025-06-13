9e2b77900c11151fff5189283d38789ca7e9d4b3
JerryLin083 <stabber5760@gmail.com>
2025-06-13 21:01:06 +0800

    change svelte to solidjs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(() => {
+  return {
+    plugins: [solid()],
+    build: {
+      assetsInlineLimit: 0,
+      outDir: "../backend/static",
+      emptyOutDir: true,
+    },
+  };
 });
